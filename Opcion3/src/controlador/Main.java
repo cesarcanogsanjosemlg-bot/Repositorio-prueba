fe5b52527608e2c3e9f3d63d573528aaa9bd9d72
Ricardo17V <ricardoebhotemenv.sanjosemlg@fundacionloyola.net>
2026-02-11 12:43:11 +0100

    Update Main.java

--- Opcion3/src/controlador/Main.java
+++ Opcion3/src/controlador/Main.java
@@ -1,22 +1,11 @@
 package controlador;
 /**
- * @authors César Cano González / Ricardo
+ * @authors César Cano González / Ricardo Ebhotemen Vázquez
  * @fecha 11/02/2026
  * @version 1.0.0
  */
-import javax.swing.SwingUtilities;
-
-import vista.Vista;
-
 public class Main {
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				Vista v = new Vista();
-				Controlador c = new Controlador();
-				v.setVisible(true);
-			}
-		});
-	}
+    public static void main(String[] args) {
+        new Controlador().arrancar();
+    }
 }
