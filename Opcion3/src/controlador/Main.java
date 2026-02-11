212ad6ace1533026cca0da012462a1bfd0a51419
Ricardo17V <ricardoebhotemenv.sanjosemlg@fundacionloyola.net>
2026-02-11 14:04:46 +0100

    Update Main.java

--- Opcion3/src/controlador/Main.java
+++ Opcion3/src/controlador/Main.java
@@ -1,9 +1,10 @@
-package controlador;
 /**
  * @authors César Cano González / Ricardo Ebhotemen Vázquez
  * @fecha 11/02/2026
  * @version 1.0.0
  */
+package controlador;
+
 public class Main {
     public static void main(String[] args) {
         new Controlador().arrancar();
