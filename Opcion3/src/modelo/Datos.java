636a3767c87584a46e9dce99533900c1dd3b5908
Ricardo17V <ricardoebhotemenv.sanjosemlg@fundacionloyola.net>
2026-02-11 12:10:27 +0100

    Update and rename Modelo.java to Datos.java
    Clase de los datos utilizados en el programa de la opción 3 del examen de dom

---
+++ Opcion3/src/modelo/Datos.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Datos {
+    public String textoBotonCrear = "Crear caja";
+    public String textoBotonOcultar = "Ocultar Contenido";
+    public String textoBotonMostrar = "Mostrar Contenido";
+    public String textoDefault = "pulsa el boton para borrar";
+    
+    public String textoGuardado = textoDefault;
+    public boolean existe = false;
+}
