8205ddb9631f593102dc80b6a0226524d32a2b26
Ricardo17V <ricardoebhotemenv.sanjosemlg@fundacionloyola.net>
2026-02-11 12:19:37 +0100

    Update Datos.java
    Añadido del ancho de la caja

--- Opcion3/src/modelo/Datos.java
+++ Opcion3/src/modelo/Datos.java
@@ -4,7 +4,8 @@ public class Datos {
 
     public int anchoVentana = 400;
 	public int altoVentana = 200;
-    
+    public int anchoCaja = 20;
+	
     public String textoBotonCrear = "Crear caja";
     public String textoBotonOcultar = "Ocultar Contenido";
     public String textoBotonMostrar = "Mostrar Contenido";
