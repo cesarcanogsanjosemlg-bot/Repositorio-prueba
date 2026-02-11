51b5215f557e09bb50645dec92591e8b3e5518fe
Ricardo17V <ricardoebhotemenv.sanjosemlg@fundacionloyola.net>
2026-02-11 12:22:25 +0100

    Update Datos.java

--- Opcion3/src/modelo/Datos.java
+++ Opcion3/src/modelo/Datos.java
@@ -9,8 +9,7 @@ public class Datos {
     public String textoBotonCrear = "Crear caja";
     public String textoBotonOcultar = "Ocultar Contenido";
     public String textoBotonMostrar = "Mostrar Contenido";
-    public String textoDefault = "pulsa el boton para borrar";
+    public String textoGuardado = "pulsa el boton para borrar";
     
-    public String textoGuardado = textoDefault;
     public boolean existe = false;
 }
